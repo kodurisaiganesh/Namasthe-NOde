402d3597a259739a58d04b2e9e5ffb4cf0408feb
Koduri Sai Ganesh <kodurisaiganesh@gmail.com>
2025-11-29 11:03:28 +0530

    ep-004 dione

--- src/app.js
+++ src/app.js
@@ -8,18 +8,7 @@ server.get("/user/:userid",(req,res)=>
     console.log(req.params);
     res.send("Welcome to the User");
 })
-server.post("/user",(req,res)=>
-{
-    res.send("Data Posted Successfully");
-})
-server.delete("/user",(req,res)=>
-{
-    res.send("Delete Successfully");
-})
-server.use("/",(req,res)=>
-{
-    res.send("Hello")
-})
+
 server.listen(4000,()=>
 {
     console.log("Server running");
