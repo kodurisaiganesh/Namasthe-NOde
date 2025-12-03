513d93d7514710a696da5846aa6f47498f20764f
Koduri Sai Ganesh <kodurisaiganesh@gmail.com>
2025-12-03 19:36:05 +0530

    get API'S

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const Mongodb=require('./config/database')
 server.use(express.json())
 server.post('/signup',async(req,res)=>
 {
-   const user=new User(req.body());
+   const user=new User(req.body);
 
    try{
    await user.save();
@@ -18,6 +18,51 @@ server.post('/signup',async(req,res)=>
    {
         res.status(400).send("Something Went Wrong");
    }
+})
+
+server.get("/getdata",async(req,res)=>
+{
+    const mailid=req.body.email;
+    try{
+    const user=await User.find({email:mailid});
+    if(user.length===0)
+    {
+        res.send("User not found");
+    }
+    else{
+        res.status(404).send(user)
+    }
+    }
+    catch(err)
+    {
+        res.status(400).send("SomeThing Went Wrong");
+    }
+})
+
+server.get('/feeds',async(req,res)=>{
+    const a=req.body.email;
+    try
+    {
+        const mailid=await User.findOne({email:a});
+        res.send(mailid);
+    }
+    catch(err)
+    {
+        res.status(404).send("Something Went Wrong");
+    }
+})
+
+server.get('/feed',async(req,res)=>
+{
+    try
+    {
+        const email= await User.find({});
+        res.send(email)
+        
+    }
+    catch{
+        res.status(404).send("Something went wrong");
+    }
 })
 
 Mongodb().then(()=>
